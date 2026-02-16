18e3ddf398b39fbb870a98820ed6bdc3d5edcf5b
Keita <fmb.vb.1996@gmail.com>
2026-02-16 21:19:07 +0900

    設問5

---
+++ send.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせフォーム - 送信完了画面</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>お問い合わせフォーム - 送信完了画面</h1>
+    </header>
+    <div class="rink">
+        <a href="http://localhost:8888/yamaji-keita_STEP6/contact.php">
+            お問い合わせフォームに戻る 
+        </a>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
